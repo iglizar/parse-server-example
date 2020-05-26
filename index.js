bd4d6973e210d924b9c967a80345f874cb90e03e
iglizar <38776942+iglizar@users.noreply.github.com>
2020-05-26 10:41:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: 'mongodb://ilizarraga:iilwyB91@ds337377.mlab.com:37377/instagram',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
